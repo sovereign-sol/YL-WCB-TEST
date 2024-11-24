2f0337aebaaac7133b63f588d7fa9dacd4482b7d
sovereign-sol <146667357+sovereign-sol@users.noreply.github.com>
2024-11-24 17:17:20 -0500

    Add files via upload
    Add some checks

--- src/Components/Staking/Staking.jsx
+++ src/Components/Staking/Staking.jsx
@@ -7,14 +7,13 @@ import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 // import { transact, Web3MobileWallet } from '@solana-mobile/mobile-wallet-adapter-protocol-web3js';
 
 
-
 const Staking = ({ toastMessage }) => {
   const { connection } = useConnection();
   const wallet = useWallet();
   const [amount, setamount] = useState('0');
   const [balance, setBalance] = useState(null);
   const [solToStake, setSolToStake] = useState('0')
-  const fee = 0.001;
+  const fee = 0.01;
 
   useEffect(() => {
     localStorage.removeItem('walletName');
@@ -58,7 +57,7 @@ const Staking = ({ toastMessage }) => {
           toastMessage('Ops! Something went wrong. Please check balance and/or permissions','error')
         }
       } catch (e) {
-        toastMessage(e.message , 'error')
+        toastMessage(/*e.message*/'Transaction cancelled' , 'error')
       }
     }
     else {
@@ -67,12 +66,15 @@ const Staking = ({ toastMessage }) => {
   };
 
   useEffect(() => {
-    setamount(() => amount)
-  const _amount = parseFloat(amount) + fee;
+    //setamount(() => amount)
+    if (amount === "") {
+      setSolToStake(0);
+      return;
+    }
+  //const _amount = parseFloat(amount) + fee;
     if (balance > 0) {
-      if (_amount > balance) {
-        amount == balance && toastMessage('Transaction not possible: You requested to stake '+amount+' SOL, which, together with the fee, exceeds your balance of '+balance+' SOL.','error');
-        amount > balance && toastMessage('Transaction not possible: You requested to stake '+amount+' SOL, which, exceeds your balance of '+balance+' SOL.','error');
+      if (amount > (balance - fee)) {
+        toastMessage('Transaction not possible: You requested to stake '+amount+' SOL, which, exceeds your balance of '+balance+' SOL.','error');
       } else {
         setSolToStake(() => parseFloat(amount))
       }
@@ -83,8 +85,8 @@ const Staking = ({ toastMessage }) => {
   useEffect(() => {
     if (balance > 0) {
       const solToStakeValue = balance - fee;
-      setSolToStake(parseFloat(solToStakeValue.toFixed(4)));
-      setamount(parseFloat(solToStakeValue.toFixed(4)));
+      setSolToStake(parseFloat(solToStakeValue));
+      setamount(parseFloat(solToStakeValue));
     }else{
       setamount(0)
       setSolToStake(0);
@@ -109,14 +111,29 @@ const Staking = ({ toastMessage }) => {
 
   return (
     <>
+    {wallet.connected ? (
       <div className='w-fit p-10 rounded-lg bg-[#fff] mx-auto mt-20'>
         <img className='mb-5 mx-auto' src={fullNameLogo} alt="fullNameLogo" />
-        <div className='border-[#FF6300] border-2'>
-          <input onChange={(e) => setamount(()=>parseFloat(e.target.value))} value={amount} className='outline-none py-1 px-2 text-lg w-full' type="number" min={0} name="amount" placeholder='0' />
-          <button onClick={() => setamount(() => parseFloat(balance - fee))} className='w-full px-2 bg-[#FF6300] transition-all hover:opacity-80 text-white'>Maximum Amount</button>
+        <div className='border-2 rounded-lg' style={{ borderColor: '#FF6300', borderWidth: '2px', backgroundColor: '#fff' }}>
+          <input onChange={(e) => {const value = e.target.value; setamount(() => value === "" ? "" : parseFloat(value));}} value={amount} className='outline-none py-1 px-2 text-lg w-full text-center' type="number" min={0} name="amount" placeholder='0' />
         </div>
-        <button  className={`text-[#0047AB] border-[#0047AB] hover:scale-10 hover:text-[#FF6300] hover:border-[#FF6300] border-2 p-2 w-full mt-5 font-semibold transition-all flex justify-center items-center `} onClick={onSendClick}>Stake with <img className='w-8 ml-3' src={logo} alt={logo} /></button>
+        <button onClick={() => setamount(() => parseFloat(balance - fee))}
+        className='w-auto px-2 bg-[#FF6300] transition-all hover:opacity-80 text-white justify-center items-center rounded-lg mx-auto block'>
+          Max Amount
+        </button>
+        <button
+          className={`text-[#FFFFFF] bg-[#0047AB] hover:scale-10 border-2 p-2 w-auto mt-5 font-semibold transition-all hover:opacity-80 text-white flex justify-center items-center rounded-lg mx-auto disabled:bg-gray-400`}
+          onClick={onSendClick}
+          disabled={!wallet.connected || amount <= 0 || amount > (balance - fee) || amount === ''}
+        >
+          Stake with us
+        </button>
       </div>
+      ) : (
+        <div className='w-fit p-10 rounded-lg bg-[#fff] mx-auto mt-20'>
+        <p className='text-center text-red-500'>Please select your wallet</p>
+        </div>
+        )}
     </>
   )
 }
